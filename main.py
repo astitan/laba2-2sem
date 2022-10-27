bbc829485c1bd52774f474e234b9ee0bf7121e47
astitan <99386386+astitan@users.noreply.github.com>
2022-10-27 19:01:05 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ while (T < 1) or (T > 20):
 X = np.zeros((K,K), dtype = int)
 for i in range(K):                                                      #заполняем матрицу X
     for j in range(K):
-        X[i][j] = random.randint(-5,5)
+        X[i][j] = random.randint(-1,3)
 while np.linalg.matrix_rank(X) != K:
     for i in range(K):                                                  # заполняем матрицу снова X, если ранг не соответствует заданному
         for j in range(K):
@@ -19,25 +19,23 @@ while np.linalg.matrix_rank(X) != K:
 current_matrix = np.zeros((K,K), dtype = int)
 current_matrix = X
 sum = 0
-def matrix(n,current_matrix):
+current_fakt = 1
+for n in range(1, 50):
     if n == 1:
         current_matrix = X
-    if n > 1:
-        current_matrix = np.dot(current_matrix, X)
-        current_matrix = np.dot(current_matrix,X)
-    return current_matrix
-current_fakt = 1
-def faktorial(n,current_fakt):
-    if n == 1:
         current_fakt = 1
-    elif n == 2:
-        current_fakt = current_fakt * n * 3
-    else:
-        current_fakt = current_fakt * (2*n-2) * (2*n-1)
-    return current_fakt
-for n in range(1,10):
-    sum = sum + (-1)**(2*n-1) * np.linalg.det(matrix(n,current_matrix)) / faktorial(n,current_fakt)
-    print(n)
+    if n > 1:
+        if n == 2:
+            current_fakt = current_fakt * n * 3
+        else:
+            current_fakt = current_fakt * (2*n - 2) * (2 * n - 1)
+        current_matrix = np.dot(current_matrix, X)
+        current_matrix = np.dot(current_matrix, X)
+    sum = sum + (-1)**(n-1) * np.linalg.det(current_matrix) / current_fakt
+    print("n: ",n)
+    print("matrix: ",np.linalg.det(current_matrix))
+    print("fakt: ",current_fakt)
+    print("sum: ",sum)
     if abs(sum) <= 10**(-T):
         print("Сумма ЗЧР равна: ", sum)
         break
